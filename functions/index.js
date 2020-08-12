f9192eb2d9df0ccfd7fb1cea582bd1c97c9c3d66
cpulover <justacpulover@gmail.com>
2020-08-12 15:20:01 +0200

    ENHANCE: Auth triggers for signing up and deleting user

--- functions/index.js
+++ functions/index.js
@@ -8,12 +8,6 @@ const functions = require('firebase-functions');
 //   response.send("Hello from Firebase!");
 // });
 
-//http request 1
-exports.randomNumber = functions.https.onRequest((request, response)=>{
-    const number = Math.round(Math.random()*100);
-    console.log("Generated number: "+number);
-    response.send(number.toString());
-});
 
 //http request 2
 exports.myWebsite = functions.https.onRequest((request, response)=>{
@@ -25,3 +19,13 @@ exports.sayHello=functions.https.onCall((data, context)=>{
     const name=data.name;
     return `hello ${name}!!!`;
 })
+
+// auth trigger (new user signup)
+exports.newUserSignUp = functions.auth.user().onCreate(user => {
+    console.log('user created: ', user.email, user.uid);
+  });
+  
+  // auth trigger (user deleted)
+  exports.userDeleted = functions.auth.user().onDelete(user => {
+    console.log('user deleted: ', user.email, user.uid);
+  });
